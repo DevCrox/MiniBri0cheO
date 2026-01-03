a6b8bb2999b084dabc675c9455f6c85058b4b37c
DevCrox <crocmou370@gmail.com>
2026-01-03 18:21:44 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // 'base' set to './' ensures assets are loaded correctly regardless of the subdirectory 
-  // the site is deployed to (e.g. username.github.io/repo-name/)
-  base: './', 
-  build: {
-    outDir: 'dist',
-  }
+  base: '/MiniBri0cheO/',
+})
 });
